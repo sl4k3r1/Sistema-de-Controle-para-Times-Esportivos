6108846116c6a931462cba181dee35da5c61f899
Bernardo Mattos Netto <61763229+sl4k3r1@users.noreply.github.com>
2021-05-06 13:45:45 -0300

    Update Jogador.cpp

--- Sistema de Controle para Times Esportivos/Jogador.cpp
+++ Sistema de Controle para Times Esportivos/Jogador.cpp
@@ -2,16 +2,16 @@
 	
 int Jogador::ultimoNumJogador = 0;
 
-Jogador::Jogador(string n, float s)
+Jogador::Jogador(string nome, float s)
 {
-    this->nome = n;
+    this->nome = nome;
     this->numJogador = geraNumJogador();
     salario = s >= 0 ? s : 0.0;
 }
-	
+
 void Jogador::JogadorImprime()
 {
-    cout << numJogador << " - " << nome << " - " << salario << endl;
+    cout << numJogador << " - " << nome << " - " << salario <<" - " << areaDeAtuacao<< endl;
 }
 
 int Jogador::geraNumJogador()
@@ -39,3 +39,49 @@ void Jogador::setSalario(float s)
 {
     salario = s >= 0 ? s : 0.0;
 }
+
+void Jogador::setSalarioLiquido()
+{
+    float inss=0;
+    float desc=0;
+    if(salario = 1100,00){
+        salarioL=salario-(salario*7.5)/100;
+    }else if(salario = 2000,00){
+        inss= (salario*9)/100;
+        desc= (salario*7,5)/100;
+        salarioL=(salario-desc)-inss;
+    }else if(salario = 3100,00){
+        inss= (salario*12)/100;
+        desc= (salario*15)/100;
+        salarioL=(salario-desc)-inss;
+    }else if(salario = 4100,00){
+        inss= (salario*14)/100;
+        desc= (salario*22)/100;
+        salarioL=(salario-desc)-inss;
+    }else if(salario > 4100,00){
+        inss= (salario*14)/100;
+        desc= (salario*27)/100;
+        salarioL=(salario-desc)-inss;
+    }
+}
+
+float Jogador::getSalarioL()
+{
+    return salarioL;
+}
+
+string Jogador::getNacionalidade()
+{
+    return nacionalidade;
+}
+
+string Jogador::getAreaDeAtuacao()
+{
+    return areaDeAtuacao;
+}
+
+/*int Jogador::getDataDeNacimento()
+{
+    return dataDeNacimento;
+}
+*/
